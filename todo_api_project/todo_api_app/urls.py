53c5174fcb2b93b11c47a53c014adca70f093579
Edward Kemper <edwardkemper@gmail.com>
2023-11-19 16:00:39 -0700

    working details view

--- todo_api_project/todo_api_app/urls.py
+++ todo_api_project/todo_api_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import (
+    TodoDetailApiView,
     TodoListApiView,
 )
 
 urlpatterns = [
     path('api', TodoListApiView.as_view()),
+    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
 ]
