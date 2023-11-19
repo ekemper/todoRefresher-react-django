836b615f39073e40b8d0e11b5961ec6d7b5f1986
Edward Kemper <edwardkemper@gmail.com>
2023-11-19 14:56:01 -0700

    wtf

--- api/todos/migrations/0001_initial.py
+++ api/todos/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Snippet',
+            name='Todo',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True)),
@@ -24,3 +24,4 @@ class Migration(migrations.Migration):
             },
         ),
     ]
+
